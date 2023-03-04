328b620e646daee811242c9209d83e970e11bb4e
Chih Liao <support@mainmining.com>
2023-03-04 02:13:15 +0100

    aboutedit

--- minting-dapp/src/scripts/react/components/AboutChanGirl.tsx
+++ minting-dapp/src/scripts/react/components/AboutChanGirl.tsx
@@ -7,25 +7,30 @@ function AboutChanGirl() {
     
     return(
         <div className="no-wallet">
-        <div className='green2'>
-          Introducing CHANGIRL: a collection featuring meticulously crafted digital artworks of a uniquely designed anime-character 
-          that resembles the aesthetics and comely of 4Chan culture. 
-          <br/>
-          <br/>
-          Each NFT is a one-of-a-kind distinctive artwork, carefully crafted by our team of talented and visionary artists, 
-          making it a truly special addition to your NFT collection. 
-          <br/>
-          <br/>
-          Not only that, but each NFT purchased during the minting process also carries a significant chance of increased rarity, 
-          something of which is often subjected to higher prices in the marketplace as these artworks are more attractive to NFT collectors and traders.
-          <br/>
-          <br/>
-          The collection consists of 1000 unique NFTs at only 0.08 ETH each! However, in order to ensure competitive trading prices at the marketplace
-          the minting process will be closed before the entire collection is minted.
-          <br/>
-          <br/>
-          The minting process may in other words be <strong>ceased at ANY TIME.</strong>
-        </div>
+          <div className='green2'>
+            Introducing CHANGIRL: a collection featuring meticulously crafted digital artworks of a uniquely designed anime-character 
+            that resembles the aesthetics and comely of 4Chan culture. 
+            <br/>
+            <br/>
+            Each NFT is a one-of-a-kind distinctive artwork, carefully crafted by our team of talented and visionary artists, 
+            making it a truly special addition to your NFT collection. 
+            <br/>
+            <br/>
+            But we're not just creating beautiful art. 
+            Our NFTs are also designed to be a smart investment. 
+            We're using data analytics and market insights to create NFTs that are not only aesthetically stunning 
+            but also valuable collectibles that will appreciate in value over time.
+            Each ChanGirl NFT purchased during the minting process also comes with an opportunity for increased rarity. 
+            This means that not only are you getting a one-of-a-kind digital artwork, but also a type of artwork which will have the potential 
+            to perform at much higher prices in the marketplace, as scarcity and rarity is more attractive to NFT collectors and traders.
+            <br/>
+            <br/>
+            The collection consists of 1000 unique NFTs at only 0.08 ETH each! However, in order to ensure competitive trading prices at the marketplace,
+            the minting process will be permanently closed before the entire collection is sold out.
+            <br/>
+            <br/>
+            The minting process may in other words be <strong>ceased at ANY TIME.</strong>
+          </div>
         </div>
     )
 }
